41c0dec7581ea6fe9ed421686abf25fc13fb2685
Mohamed Oulmouden <106393280+oulmdev@users.noreply.github.com>
2024-02-13 19:19:12 +0100

    ex00 done

---
+++ Mobile - 0 - Basic of the mobile application/mobileModule00/ex00/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Text('A simple text',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print('Button clicked');
+            },
+            child: const Text(
+              'Click me',
+              style: TextStyle(color: Colors.black),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
